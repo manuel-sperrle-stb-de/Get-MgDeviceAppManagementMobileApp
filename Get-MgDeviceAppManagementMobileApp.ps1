2b70507935e73a891b2891d2977ffb814afc81a0
manuel-sperrle-stb-de <manuel.sperrle@stb.de>
2023-08-23 14:58:29 +0200

    added: DefaultParameterValues added: other filtered flat output options

--- Get-MgDeviceAppManagementMobileApp.ps1
+++ Get-MgDeviceAppManagementMobileApp.ps1
@@ -17,13 +17,19 @@ $ConnectMgGraphParams = @{
 Select-MgProfile -Name beta
 Connect-MgGraph @ConnectMgGraphParams
 
+$PSDefaultParameterValues['*:Encoding']='utf8'
+$PSDefaultParameterValues['*:UseCulture']=$true
+$PSDefaultParameterValues['*:NoTypeInformation']=$true
+$PSDefaultParameterValues['Export*:Force']=$true
+$PSDefaultParameterValues['Out-File:Force']=$true
+
 # All
 $MgDeviceAppManagementMobileApp = Get-MgDeviceAppManagementMobileApp -All -Property Assignments -ExpandProperty Assignments
-$MgDeviceAppManagementMobileApp | ConvertTo-Json -Depth 4 | Out-File '.\MgDeviceAppManagementMobileApp.json' -Force
+$MgDeviceAppManagementMobileApp | ConvertTo-Json -Depth 4 | Out-File '.\MgDeviceAppManagementMobileApp.json'
 
 # Filtered
 $MgDeviceAppManagementMobileAppFiltered = $MgDeviceAppManagementMobileApp | Where-Object { $_.AdditionalProperties.'@odata.type' -in $ODataType }
-$MgDeviceAppManagementMobileAppFiltered | ConvertTo-Json -Depth 4 | Out-File '.\MgDeviceAppManagementMobileApp.filtered.json' -Force
+$MgDeviceAppManagementMobileAppFiltered | ConvertTo-Json -Depth 4 | Out-File '.\MgDeviceAppManagementMobileApp.filtered.json'
 
 # Filterered Flat
 $SelectObjectParams = @{
@@ -57,7 +63,10 @@ $SelectObjectParams = @{
 }
 
 $MgDeviceAppManagementMobileAppFilteredFlat = $MgDeviceAppManagementMobileAppFiltered | Select-Object @SelectObjectParams
-$MgDeviceAppManagementMobileAppFilteredFlat | Export-Csv '.\MgDeviceAppManagementMobileApp.filtered.csv' -UseCulture -Encoding utf8 -NoTypeInformation -Force
-$MgDeviceAppManagementMobileAppFilteredFlat | Where-Object { $_.'Assignments.Intent' -eq 'required' } | Export-Csv '.\MgDeviceAppManagementMobileApp.filtered.required.csv' -UseCulture -Encoding utf8 -NoTypeInformation -Force
-$MgDeviceAppManagementMobileAppFilteredFlat | Where-Object { $_.'Assignments.Intent' -eq 'required' -and $_.'AdditionalProperties.@odata.type' -eq '#microsoft.graph.winGetApp' } | Export-Csv '.\MgDeviceAppManagementMobileApp.filtered.required.winGetApp.csv' -UseCulture -Encoding utf8 -NoTypeInformation -Force
-$MgDeviceAppManagementMobileAppFilteredFlat | Where-Object { $_.'Assignments.Intent' -eq 'required' -and $_.'AdditionalProperties.@odata.type' -eq '#microsoft.graph.win32LobApp' } | Export-Csv '.\MgDeviceAppManagementMobileApp.filtered.required.win32LobApp.csv' -UseCulture -Encoding utf8 -NoTypeInformation -Force
+$MgDeviceAppManagementMobileAppFilteredFlat | Export-Csv '.\MgDeviceAppManagementMobileApp.filtered.csv'
+$MgDeviceAppManagementMobileAppFilteredFlat | Where-Object { $_.'AdditionalProperties.@odata.type' -eq '#microsoft.graph.winGetApp' } | Export-Csv '.\MgDeviceAppManagementMobileApp.filtered.winGetApp.csv'
+$MgDeviceAppManagementMobileAppFilteredFlat | Where-Object { $_.'AdditionalProperties.@odata.type' -eq '#microsoft.graph.win32LobApp' } | Export-Csv '.\MgDeviceAppManagementMobileApp.filtered.win32LobApp.csv'
+
+$MgDeviceAppManagementMobileAppFilteredFlat | Where-Object { $_.'Assignments.Intent' -eq 'required' } | Export-Csv '.\MgDeviceAppManagementMobileApp.filtered.required.csv'
+$MgDeviceAppManagementMobileAppFilteredFlat | Where-Object { $_.'Assignments.Intent' -eq 'required' -and $_.'AdditionalProperties.@odata.type' -eq '#microsoft.graph.winGetApp' } | Export-Csv '.\MgDeviceAppManagementMobileApp.filtered.required.winGetApp.csv'
+$MgDeviceAppManagementMobileAppFilteredFlat | Where-Object { $_.'Assignments.Intent' -eq 'required' -and $_.'AdditionalProperties.@odata.type' -eq '#microsoft.graph.win32LobApp' } | Export-Csv '.\MgDeviceAppManagementMobileApp.filtered.required.win32LobApp.csv'
